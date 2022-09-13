ac417ffb685afc21b31a777c3946f8c1246d4b0c
ömer <omerbulut1995@gmail.com>
2022-09-14 00:16:21 +0300

    new: locales kalsörü eklendi, çoklu dile çevirilebilir hale getirildi.

--- translate.dart
+++ translate.dart
@@ -7,23 +7,34 @@ void main() async {
 
   File willTranslateFile = File('will_translate.json');
   File translatedFile = File('translated.json');
+  String fromLaunguage = 'tr';
+  List translateLaunguages = ['en', 'nl', 'ru'];
 
   final willTranslateString = await willTranslateFile.readAsString();
   final Map<String, dynamic> willTranslateJson = jsonDecode(willTranslateString);
 
   Map<String, dynamic> newJson = {};
 
-  int count = 1;
-  for (var element in willTranslateJson.entries) {
-    try {
-      var translation = await translator.translate(element.value, from: 'en', to: 'tr');
-      newJson.addAll({element.key: translation.text});
-      print('$count : $translation');
-    } catch (e) {
-      print(e.toString());
+  for (var launguage in translateLaunguages) {
+    int count = 1;
+    for (var element in willTranslateJson.entries) {
+      try {
+        var translation = await translator.translate(element.value, from: fromLaunguage, to: launguage);
+        newJson.addAll({element.key: translation.text});
+        print('$count : $translation');
+      } catch (e) {
+        print(e.toString());
+      }
+      count++;
     }
-    count++;
+    translatedFile = File('locales/$launguage.json');
+
+    final encoded = getPrettyJSONString(newJson);
+    await translatedFile.writeAsString(encoded);
   }
-  final encoded = jsonEncode(newJson);
-  await translatedFile.writeAsString(encoded);
+}
+
+String getPrettyJSONString(jsonObject) {
+  var encoder = new JsonEncoder.withIndent("     ");
+  return encoder.convert(jsonObject);
 }
